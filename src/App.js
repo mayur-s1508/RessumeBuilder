66fb5c25d05985b3184e5766a466478110aa29ec
Mayur Sonawane <mayursonawane54321@gmail.com>
2020-08-01 20:34:50 +0530

    'Basicressume'

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p className="head"><u>NIKHIL NANDKUMAR WAYKOS</u></p>
+      <p className="left_sub_head"><b>Email Id:</b> waykosn@gmail.com</p>
+      <p className="right_sub_head"><b>Contact No.:</b> +91 95116 64263</p>
+      <p className="left_sub_head"><b>LinkedIn ID:</b> wwww.linkedin.com/in/nikhil-waykos-083610146</p>
+      <p className="right_sub_head"><b>Address:</b> Aurangabad, MH, India.</p>
+      <hr />
+      <p className="main_sub_head">ACADEMIA</p>
+      <hr />
+      <p className="main_sub_head"><b>PROJECT DETAILS</b></p>
+      <ul className="unorder_list">
+        <li>GIZ MASSIA Project (2019-20) – Static Website Development URL: www.omsaiindustries.com</li>
+        <li>Attendance Monitoring System Using PHP (2019-20) – Mini Project (Database System)</li>
+        <li>Python Project (2018-19) – Drone security management, Securing drone system by providing encryption to real time image captured by drone using python</li>
+        <li>Result Analysis Using R (2018–19) – Result calculation and analysis with graphs (simple Gui app)</li>
+        <li>Website Project (2018-19) – Mini Project for Vegetables Shopping Site (HTML5, CSS, JS)</li>
+        <li><ul typeof="circle">Freelancing In Static Website Development (Team project):
+          <li>Kaizeninfotek – URL: www.kaizeninfotek.com</li>
+          <li>TeckyTrick – URL: www.teckytrick.com</li>
+          <li>AnkurGECA – URL : ankur.geca.ac.in/</li>
+          </ul></li>
+      </ul>
+      <hr />
+      <p className="main_sub_head">CERTIFICATION, TECHNICAL SKILLS & LEARNING</p>
+      <ul className="unorder_list">
+        <li>Angular Internship at Modi Innovations, Aurangabad. – 30 days duration</li>
+        <li>REDHAT RHCSA (2019) – Score: <u>300/300</u></li>
+        <li>NPTEL 2019 - <u>Topper in 5%</u> Database Management System - 85%</li>
+        <li>NPTEL 2019 - <u>Silver Elite</u> Joy of computing using python - 82%</li>
+        <li>NPTEL 2018 - <u>Topper in 2%</u> Introduction to programming in C - 87%</li>
+        <li>NPTEL 2018 – Passed Programming, Algorithms & Data Structures using Python - 40%</li>
+        <li>“Certificate of Completion of C Training ” offered by Spoken Tutorial Project, IIT Bombay, funded by National Mission on Education through ICT, MHRD, Govt., of India.</li>
+        <li>Coursera Introduction to TensorFlow for Artificial Intelligence, Machine Learning and Deep Learning</li>
+        <li>Coursera Introduction and Programming with IoT Boards</li>
+      </ul>
+      <hr />
+      <p className="main_sub_head">PARTICIPATED ACTIVITIES AND ACHIEVEMENTS</p>
+      <ul className="unorder_list">
+        <li>Participated in the workshop on “The Internet of Things” conducted by Geekslab Technologies Pvt. Ltd. In association with AIESEC IIT-Delhi, held on 12th & 13th February 2019.</li>
+        <li>Participation in Google Code Jam 2019</li>
+        <li>Participation in ACM ICPC 2019-20</li>
+      </ul>
+      <hr />
+      <p className="main_sub_head">PERSONAL INFORMATION</p>
+      <p>I do hereby confirm that the above information is true to the best of my knowledge and belief</p>
+      <pre>Date: 27-July-2020        Place: Aurangabad          Sign: </pre>
+      <hr />
+    </div>
+  )   ;
+}
+
+export default App;
---
+++ src/components/table.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MaterialTable from 'materailtable'
+export const Table=()=>{
+    return(<div>
+        </div>)
+        
+
+}
